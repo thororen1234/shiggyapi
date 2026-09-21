30f955dc4c5b253107f47fb78a43305c3a2c2c2e
thororen <78185467+thororen1234@users.noreply.github.com>
2026-09-21 15:42:18 -0400

    fix

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,13 @@ const nextConfig: NextConfig = {
     output: "standalone",
     outputFileTracingIncludes: {
         "/*": [
-            "./node_modules/.pnpm/@img+sharp*/**/*",
+            "./node_modules/.pnpm/@img+sharp*/**/*.node",
+            "./node_modules/.pnpm/@img+sharp*/**/*.so*",
+            "./node_modules/.pnpm/@img+sharp*/**/*.dylib",
+            "./node_modules/.pnpm/@img+sharp*/**/*.dll",
+            "./node_modules/.pnpm/@img+sharp*/**/*.cjs",
+            "./node_modules/.pnpm/@img+sharp*/**/*.js",
+            "./node_modules/.pnpm/@img+sharp*/**/*.json",
             "./src/assets/petpet.png",
         ],
     },
